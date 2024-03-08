afef57b0035d4eecd26113b354a46ae6f21cbcd1
aggarjgov <jaspaul.aggarwal1@cabinetoffice.gov.uk>
2024-03-08 14:06:10 +0000

    utils

---
+++ src/utils/object.utils.ts
@@ -0,0 +1,12 @@
+export const filterByObject = <T, G>(data: T[], filterBy: G[]): T[] => {
+  if (Array.isArray(data))
+    return data.filter(function (i) {
+      return filterBy.some(function (j) {
+        return !Object.keys(j).some(function (prop) {
+          return i[prop] !== j[prop];
+        });
+      });
+    });
+
+  return data;
+};
