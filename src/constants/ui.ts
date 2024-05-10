2306ea6268efed8dafca8216332b954d4ee2a775
aggarjgov <jaspaul.aggarwal1@cabinetoffice.gov.uk>
2024-05-10 09:16:12 +0100

    const

---
+++ src/constants/ui.ts
@@ -0,0 +1,20 @@
+/**
+ * UI constants for design attributes
+ * https://design-system.service.gov.uk/styles/
+ */
+
+export const sizes = {
+  small: "s",
+  medium: "m",
+  large: "l",
+  "extra-large": "xl",
+} as const;
+
+export const layout = {
+  full: "full",
+  "three-quarters": "three-quarters",
+  "two-thirds": "two-thirds",
+  "one-half": "one-half",
+  "one-third": "one-third",
+  "one-quarter": "one-quarter",
+} as const;
