1f4856b831ced58ba861e93772253c9a8e4a7157
Jas <webmaster@jaspaul.com>
2024-06-28 14:25:55 +0100

    saved

--- src/utils/file.utils.test.ts
+++ src/utils/file.utils.test.ts
@@ -1,31 +1,38 @@
 import { trimCsvValues } from './file.utils'
 
 describe('trimCsvValues', () => {
-  it('should trim leading and trailing spaces from values', () => {
+  it('trim leading and trailing spaces from values', () => {
     const inputCsv = ' name, age ,city \nJohn Doe, 28 ,New York'
     const expectedCsv = 'name,age,city\nJohn Doe,28,New York'
     expect(trimCsvValues(inputCsv)).toEqual(expectedCsv)
   })
 
-  it('should handle empty values correctly', () => {
+  it('wont trim commas inside quotes', () => {
+    const inputCsv =
+      ' name, age ,city,date\nJohn Doe, 28 ,New York,"Jan 1, 2021"'
+    const expectedCsv = 'name,age,city,date\nJohn Doe,28,New York,"Jan 1, 2021"'
+    expect(trimCsvValues(inputCsv)).toEqual(expectedCsv)
+  })
+
+  it('handle empty values correctly', () => {
     const inputCsv = 'name,age,city\n,,'
     const expectedCsv = 'name,age,city\n,,'
     expect(trimCsvValues(inputCsv)).toEqual(expectedCsv)
   })
 
-  it('should not alter rows without spaces', () => {
+  it('not alter rows without spaces', () => {
     const inputCsv = 'name,age,city\nJohn Doe,28,New York'
     const expectedCsv = 'name,age,city\nJohn Doe,28,New York'
     expect(trimCsvValues(inputCsv)).toEqual(expectedCsv)
   })
 
-  it('should handle csv with only spaces correctly', () => {
+  it('handle csv with only spaces correctly', () => {
     const inputCsv = ' , , \n , , '
     const expectedCsv = ',,\n,,'
     expect(trimCsvValues(inputCsv)).toEqual(expectedCsv)
   })
 
-  it('should handle csv with no values correctly', () => {
+  it('handle csv with no values correctly', () => {
     const inputCsv = ''
     const expectedCsv = ''
     expect(trimCsvValues(inputCsv)).toEqual(expectedCsv)
--- src/utils/file.utils.ts
+++ src/utils/file.utils.ts
@@ -1,15 +1,25 @@
-// Function to trim all values in a CSV string
 export function trimCsvValues(csvString: string): string {
+  // Placeholder for commas inside quotes
+  const placeholder = '\u{F8FF}' // Use a character unlikely to be in the input
+
   // Split the CSV string into rows
   const rows = csvString.split('\n')
 
   // Map each row
   const trimmedRows = rows.map((row) => {
+    // Replace commas inside quotes with a placeholder
+    const rowWithPlaceholders = row.replace(/"([^"]*)"/g, (match) => {
+      return match.replace(/,/g, placeholder)
+    })
+
     // Split the row into values, trim each value, then join back with commas
-    return row
+    const trimmedRow = rowWithPlaceholders
       .split(',')
       .map((value) => value.trim())
       .join(',')
+
+    // Restore commas inside quotes by replacing placeholders
+    return trimmedRow.replace(new RegExp(placeholder, 'g'), ',')
   })
 
   // Join the trimmed rows back into a single string with newline characters
